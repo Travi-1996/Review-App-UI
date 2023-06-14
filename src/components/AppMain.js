bab3a533d24b22b32863242939e81c259b988141
Ravichandran TK <ravichandran.thiyagarajan@indiumsoft.com>
2023-06-14 14:04:33 +0530

    Review-App-UI first commit

---
+++ src/components/AppMain.js
@@ -0,0 +1,142 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { makeStyles } from "@material-ui/core";
+
+const drawerWidth = 0;
+const useStyles = makeStyles((theme) => ({
+  topDrawerContent: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  mainContent: {
+    width: ({ drawerWidth }) =>
+      drawerWidth ? `calc(100% - ${drawerWidth}px)` : "100%",
+  },
+  drawerContent: {
+    width:  ({ drawerWidth }) => drawerWidth,
+    flexShrink: 0,
+    "& .MuiDrawer-paper": {
+      width:  ({ drawerWidth }) => drawerWidth,
+      boxSizing: "border-box",
+    },
+  },
+}));
+
+export default function AppMain() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [drawerWidth, setDrawerWidth] = React.useState(0);
+  const classes = useStyles({ drawerWidth });
+  const handleDrawerOpen = () => {
+    setOpen(true);
+    setDrawerWidth(240);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+    setDrawerWidth(0);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <MuiAppBar
+        position="fixed"
+        style={{ width: `calc(100% - ${drawerWidth}px)` }}
+        open={open}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </MuiAppBar>
+      <Drawer
+        className={classes.drawerContent}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <div className={classes.topDrawerContent}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <div className={classes.mainContent} open={open}>
+        <div className={classes.topDrawerContent} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+      </div>
+    </Box>
+  );
+}
---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,10 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import AppMain from "../components/AppMain";
+
+export const AppRoutes = () => {
+  return (
+    <>
+      <AppMain />
+    </>
+  );
+};
